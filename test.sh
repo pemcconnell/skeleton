e6fcab96335f175641596c6bf5aa57ab4cd2b66c
Peter McConnell <me@petermcconnell.com>
2019-10-22 20:49:52 +0100

    adding support for executing the tests inside a build container

--- test.sh
+++ test.sh
@@ -2,20 +2,38 @@
 
 set -e
 
+. includes/consts.sh
 . includes/helpers.sh
 
 log "running test suite" "heading"
-depchecker "shellcheck bats hadolint"
 
-# tests
+IN_DOCKER="${IN_DOCKER:-False}" # if True, run this script inside a container
 errs=0
-log "dockerfile lint"
-(hadolint Dockerfile && log "hadolint ...ok" "ok") || errs=1
-log "shell linting"
-(shellcheck -x ./*.sh && log "shellcheck ...ok" "ok") || errs=1
-log "shell unittests"
-bats ./unittests/ || errs=1
-log "golang unittests"
-go test -v ./... || errs=1
+
+if [ "$IN_DOCKER" = "True" ]; then
+  depchecker "docker"
+
+  docker run \
+    --rm \
+    -e "PROJECT=$PROJECT" \
+    -e "DOCKER_REGISTRY=$DOCKER_REGISTRY" \
+    -e "DOCKER_IMAGE=$DOCKER_IMAGE" \
+    -v "$(pwd):/go/" \
+    -u "$(id -u)" \
+    -w /go \
+    -ti "${DOCKER_REGISTRY}${DOCKER_IMAGE}:build" sh -c "./test.sh"
+else
+  depchecker "shellcheck bats hadolint"
+
+  # tests
+  log "dockerfile lint"
+  (hadolint Dockerfile && log "hadolint ...ok" "ok") || errs=1
+  log "shell linting"
+  (shellcheck -x ./*.sh && log "shellcheck ...ok" "ok") || errs=1
+  log "shell unittests"
+  bats ./unittests/ || errs=1
+  log "golang unittests"
+  go test -v ./... || errs=1
+fi
 
 exit $errs
